96ad142d1e5a6ea0dbbafb328674c37b378e7944
[Pioneer] Lâm Phúc Hiếu <hieu.lam@axonactive.vn>
2018-08-11 21:19:41 +0700

    Remove class

--- webContent/resources/js/classes/class.js
+++ webContent/resources/js/classes/class.js
@@ -4,16 +4,3 @@ var A = (function () {
     }
     return A;
 }());
-var B = (function () {
-    function B(child) {
-        this.child = child;
-        this.name = "Class B with child " + this.child.name;
-        this.display = function () {
-            showModal(this.name);
-        };
-    }
-    return B;
-}());
-var a = new A();
-var b = new B(a);
-//b.display();
--- webContent/resources/ts/classes/class.ts
+++ webContent/resources/ts/classes/class.ts
@@ -1,15 +1,4 @@
 class A {
     name = "Class A";
 }
-class B {
-    constructor(private child: A) {}
-    name = `Class B with child ${this.child.name}`;
-    display = function(){
-        showModal(this.name)
-    }
 
-}
-
-let a = new A();
-let b = new B(a);  
-//b.display();
